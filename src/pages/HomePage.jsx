474a27b2c8dacc9d40b72ad7f156fa421c50d5ac
BayMaxx2001 <trihien2001@gmail.com>
2022-04-14 02:29:00 +0700

    fix bug spin loading

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useUserAuthContext } from '../context/UserAuthContext';
-
+import { actionsUserAuth } from '../actions'
 
 function HomePage(props) {
     const [state, dispatch] = useUserAuthContext()
     const { user } = state
+    state.loading = false
     console.log('HomePage', state)
     return (
         <div>
